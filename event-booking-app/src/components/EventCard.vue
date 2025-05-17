5f078d60e81e183d889f8f0460fa97122a5aa711
arisdanang <arisdanang2810@gmail.com>
2025-05-17 13:41:10 +0700

    feat: styling event card

--- event-booking-app/src/components/EventCard.vue
+++ event-booking-app/src/components/EventCard.vue
@@ -1,3 +1,7 @@
 <template>
-  <p>this is component</p>
+  <div class="border border-gray-200 bg-white rounded-md">
+    <h3 class="p-4 font-medium text-xl border-b border-gray-200">ini judul</h3>
+    <p class="p-4 border-b border-gray-200">ini paragraf</p>
+    <p class="p-4">ini buat deskripsi</p>
+  </div>
 </template>
