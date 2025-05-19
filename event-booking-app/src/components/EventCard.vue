d3eb05b398d09391845903a4692cb441be0bda45
arisdanang <arisdanang2810@gmail.com>
2025-05-19 17:24:09 +0700

    fix: wrap card using RoundedCard component

--- event-booking-app/src/components/EventCard.vue
+++ event-booking-app/src/components/EventCard.vue
@@ -1,5 +1,6 @@
 <script setup>
 import ButtonCompoent from './ButtonComponent.vue'
+import RoundedCard from './RoundedCard.vue'
 defineProps({
   title: String,
   date: String,
@@ -10,12 +11,12 @@ defineEmits(['register'])
 </script>
 
 <template>
-  <div class="border border-gray-200 bg-white rounded-md">
+  <RoundedCard>
     <h3 class="p-4 font-medium text-xl border-b border-gray-200">{{ title }}</h3>
     <p class="p-4 border-b border-gray-200">{{ date }}</p>
     <p class="p-4">{{ description }}</p>
     <section class="flex justify-end p-4">
       <ButtonCompoent @click="$emit('register')">Register</ButtonCompoent>
     </section>
-  </div>
+  </RoundedCard>
 </template>
