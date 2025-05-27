66edfb6cbb051cfc7f396244a1c06c9bcff299af
arisdanang <arisdanang2810@gmail.com>
2025-05-27 16:05:38 +0700

    feat: create loading component

---
+++ event-booking-app/src/components/LoadingIndicator.vue
@@ -0,0 +1,15 @@
+<script setup>
+import RoundedCard from '@/components/RoundedCard.vue'
+</script>
+
+<template>
+  <RoundedCard class="animate-pulse">
+    <div class="p-4 border-b border-gray-200 bg-gray-300 h-8"></div>
+    <div class="p-4 border-b border-gray-200 bg-gray-300 h-6"></div>
+    <div class="p-4 bg-gray-300 h-6"></div>
+
+    <div class="flex justify-end p-4">
+      <div class="h-8 w-28 rounded-full bg-gray-300"></div>
+    </div>
+  </RoundedCard>
+</template>
