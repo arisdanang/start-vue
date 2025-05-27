c836689ccf156c339bc5ad2080e8edd7cd7c3790
arisdanang <arisdanang2810@gmail.com>
2025-05-27 16:06:51 +0700

    feat: add loading state

--- event-booking-app/src/App.vue
+++ event-booking-app/src/App.vue
@@ -1,15 +1,38 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import EventCard from '@/components/EventCard.vue'
-import BookingItem from './components/BookingItem.vue'
+import BookingItem from '@/components/BookingItem.vue'
+import LoadingIndicator from '@/components/LoadingIndicator.vue'
 
 const events = ref([])
+const isFetchingEvents = ref(false)
 
 const fetchEvents = async () => {
-  const response = await fetch('http://localhost:3001/events')
-  events.value = await response.json()
+  isFetchingEvents.value = true
+  try {
+    const response = await fetch('http://localhost:3001/events')
+    events.value = await response.json()
+  } finally {
+    isFetchingEvents.value = false
+  }
+}
 
-  console.log(events.value)
+const handleRegistration = async (event) => {
+  const newBooking = {
+    id: Date.now().toString(),
+    userId: 1,
+    eventId: event.id,
+    eventTitle: event.title,
+  }
+
+  await fetch('http://localhost:3001/bookings', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      ...newBooking,
+      status: 'confirmed',
+    }),
+  })
 }
 
 onMounted(() => fetchEvents())
@@ -20,14 +43,19 @@ onMounted(() => fetchEvents())
     <h1 class="text-4xl font-medium">Event Booking</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
     <section class="grid grid-cols-2 gap-8">
-      <EventCard
-        v-for="event in events"
-        :key="event"
-        :title="event.title"
-        :date="event.date"
-        :description="event.description"
-        @register="console.log('ini register')"
-      />
+      <template v-if="!isFetchingEvents">
+        <EventCard
+          v-for="event in events"
+          :key="event"
+          :title="event.title"
+          :date="event.date"
+          :description="event.description"
+          @register="handleRegistration(event)"
+        />
+      </template>
+      <template v-else>
+        <LoadingIndicator v-for="i in 4" :key="i" />
+      </template>
     </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-4">
