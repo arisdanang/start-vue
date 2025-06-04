e3e2deb318c2de9b90e8ecb66c33dc197eeb95ad
arisdanang <arisdanang2810@gmail.com>
2025-06-04 10:52:13 +0700

    feat: add emit

--- event-booking-app/src/components/BookingItem.vue
+++ event-booking-app/src/components/BookingItem.vue
@@ -4,14 +4,20 @@ import SectionCard from './SectionCard.vue'
 
 defineProps({
   title: String,
+  status: String,
 })
+
+defineEmits(['cancel-booking'])
 </script>
 
 <template>
   <SectionCard>
     <div class="flex justify-between">
-      <div>{{ title }}</div>
-      <ButtonComponent variant="danger"> Cancel </ButtonComponent>
+      <div class="flex space-x-3">
+        <div>{{ title }}</div>
+        <div>{{ status }}</div>
+      </div>
+      <ButtonComponent variant="danger" @click="$emit('cancel-booking')"> Cancel </ButtonComponent>
     </div>
   </SectionCard>
 </template>
