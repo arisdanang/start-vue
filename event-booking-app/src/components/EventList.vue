fcf3759b5a2a8a7f946fdbcb968e7910ebef5f2e
arisdanang <arisdanang2810@gmail.com>
2025-06-15 23:55:17 +0700

    feat: add error handling

--- event-booking-app/src/components/EventList.vue
+++ event-booking-app/src/components/EventList.vue
@@ -2,16 +2,22 @@
 import { ref, onMounted } from 'vue'
 import EventCard from '@/components/EventCard.vue'
 import LoadingIndicator from '@/components/LoadingIndicator.vue'
+import SectionCard from '@/components/SectionCard.vue'
+import ButtonComponent from './ButtonComponent.vue'
 
 defineEmits(['register'])
 const events = ref([])
 const fetching = ref(false)
+const error = ref(null)
 
 const fetchEvents = async () => {
   fetching.value = true
+  error.value = null
   try {
     const response = await fetch('http://localhost:3001/events')
     events.value = await response.json()
+  } catch (e) {
+    error.value = e
   } finally {
     fetching.value = false
   }
@@ -21,24 +27,34 @@ onMounted(() => fetchEvents())
 </script>
 
 <template>
-  <section class="grid grid-cols-2 gap-8">
-    <template v-if="!fetching">
-      <template v-if="events.length">
-        <EventCard
-          v-for="event in events"
-          :key="event"
-          :title="event.title"
-          :date="event.date"
-          :description="event.description"
-          @register="$emit('register', event)"
-        />
+  <template v-if="error">
+    <SectionCard>
+      <div class="space-y-4 flex flex-col items-center">
+        <p class="text-red-500">Could not load events at the moment. please try again.</p>
+        <ButtonComponent @click="fetchEvents">Retry now</ButtonComponent>
+      </div>
+    </SectionCard>
+  </template>
+  <template v-else>
+    <section class="grid grid-cols-1 lg:grid-cols-2 gap-8">
+      <template v-if="!fetching">
+        <template v-if="events.length">
+          <EventCard
+            v-for="event in events"
+            :key="event"
+            :title="event.title"
+            :date="event.date"
+            :description="event.description"
+            @register="$emit('register', event)"
+          />
+        </template>
+        <template v-else>
+          <div class="col-span-2 text-center text-gray-400">no events yet</div>
+        </template>
       </template>
       <template v-else>
-        <div class="col-span-2 text-center text-gray-400">no events yet</div>
+        <LoadingIndicator v-for="i in 4" :key="i" />
       </template>
-    </template>
-    <template v-else>
-      <LoadingIndicator v-for="i in 4" :key="i" />
-    </template>
-  </section>
+    </section>
+  </template>
 </template>
