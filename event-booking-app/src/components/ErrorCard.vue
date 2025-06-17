34e4d004a3b7ad43bb0512a2432281bbaf39fa17
arisdanang <arisdanang2810@gmail.com>
2025-06-17 11:49:16 +0700

    feat: add error component

---
+++ event-booking-app/src/components/ErrorCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+import SectionCard from './SectionCard.vue'
+import ButtonComponent from '@/components/ButtonComponent.vue'
+
+defineProps({
+  retry: {
+    type: Function,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <SectionCard>
+    <div class="space-y-4 flex flex-col items-center">
+      <p class="text-red-500">
+        <slot></slot>
+      </p>
+      <ButtonComponent @click="retry" v-if="retry">Retry now</ButtonComponent>
+    </div>
+  </SectionCard>
+</template>
